06f92a657eb19046ab52ad48a0927793e9551358
khaled hussien <20013@zood.ai>
2025-04-18 01:12:34 +0200

    commit

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -5,6 +5,6 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(body: Container(color: Colors.black));
   }
 }
